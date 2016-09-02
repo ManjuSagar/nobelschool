b70f729f26524f84ecab906fb66e98ffc45020fc
manju <manju@mahaswami.com>
2016-09-02 19:29:24 +0530

    Migration file added.

---
+++ db/migrate/20160902135111_create_about.rb
@@ -0,0 +1,7 @@
+class CreateAbout < ActiveRecord::Migration
+  def change
+    create_table :abouts do |t|
+      t.text :school_info
+    end
+  end
+end
--- db/migrate/20160902135119_create_about.rb
+++
@@ -1,7 +0,0 @@
-class CreateAbout < ActiveRecord::Migration
-  def change
-    # create_table :abouts do |t|
-    #   t.text :school_info
-    # end
-  end
-end
